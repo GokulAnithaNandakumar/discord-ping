a6e1660f178e7f7c2bd0812728d6930e78982408
GokulAnithaNandakumar <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2024-08-05 00:48:59 +0530

    update

--- main.go
+++ main.go
@@ -7,16 +7,21 @@ import (
 	"github.com/GokulAnithaNandakumar/discord-ping/config"
 )
 
-func main(){
-	err:=config.ReadConfig()
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Bot is running!")
+	bot.Start()
+}
 
-	if err!=nil{
+func main() {
+	err := config.ReadConfig()
+	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
-	bot.Start()
-
-	<-make(chan struct{})
-	return
+	http.HandleFunc("/", handler)
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
